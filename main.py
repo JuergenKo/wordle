4738cddd32e3f72a951487d10c46a30b027d193a
JuergenKo <141258472+JuergenKo@users.noreply.github.com>
2025-09-15 14:59:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ from kivmob import KivMob
 from kivy.app import App
 from kivy.uix.label import Label
 
-class TestApp(App):
+
+class BannerApp(App):
     def build(self):
-        return Label(text="Hello Ads")
+        ads = KivMob("ca-app-pub-3940256099942544~3347511713")  # Google test App ID
+        ads.new_banner("ca-app-pub-3940256099942544/6300978111", top_pos=False)
+        ads.request_banner()
+        ads.show_banner()
+        return Label(text="Hello with Banner!")
 
-    def on_start(self):
-        try:
-            self.ads = KivMob("ca-app-pub-3940256099942544~3347511713")  # Test App ID
-            self.ads.new_banner("ca-app-pub-3940256099942544/6300978111", top_pos=False)
-            self.ads.request_banner()
-            self.ads.show_banner()
-        except Exception as e:
-            import traceback
-            print("⚠️ AdMob init failed:", e)
-            traceback.print_exc()
 
-TestApp().run()
+if __name__ == "__main__":
+    BannerApp().run()
